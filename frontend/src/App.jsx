9dc1a1a6da0d97c6e6f0d2a12dd5c63d299219b9
roma <Shakhurin.roma@mail.ru>
2025-11-06 17:42:39 +0500

    feat: all states of the game

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,47 @@
+import { useState } from 'react'
 import Board from './components/Board.jsx'
+import initBoard from './logic/board.js'
+import { getAvailableMoves, movePiece } from "./logic/moves.js";
+
 
 export default function App() {
-  return (
-    <Board />
-  )
-}
+  const [board, setBoard] = useState(initBoard())
+  const [selectedCell, setSelectedCell] = useState(null);
+  const [availableMoves, setAvailableMoves] = useState([]);
+  const [currentPlayer, setCurrentPlayer] = useState("white");
 
+  function handleCellClick(row, col) {
+    const piece = board[row][col];
+
+    // Выбор фигуры
+    if (piece && piece.player === currentPlayer) {
+      setSelectedCell({ row, col });
+      setAvailableMoves(getAvailableMoves(board, row, col));
+      return;
+    }
+
+    // Перемещение
+    if (selectedCell) {
+      const move = availableMoves.find(m => m.row === row && m.col === col);
+      if (move) {
+        const newBoard = movePiece(board, selectedCell, move);
+        setBoard(newBoard);
+        setSelectedCell(null);
+        setAvailableMoves([]);
+        setCurrentPlayer(currentPlayer === "white" ? "black" : "white");
+      }
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <Board
+        board={board}
+        selectedCell={selectedCell}
+        availableMoves={availableMoves}
+        onCellClick={handleCellClick}
+      />
+
+    </div >
+  );
+}
