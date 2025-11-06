b669a7195315cf91a63d4b6cc314d54909c59d09
roma <Shakhurin.roma@mail.ru>
2025-11-06 17:39:26 +0500

    feat: add Peace

---
+++ frontend/src/components/Piece.jsx
@@ -0,0 +1,8 @@
+export default function Piece({ piece }) {
+  const color = piece.player === "white" ? "bg-white" : "bg-black";
+  const king = piece.isKing ? "border-4 border-yellow-300" : "";
+
+  return (
+    <div className={`w-12 h-12 rounded-full ${color} ${king}`} />
+  );
+}
